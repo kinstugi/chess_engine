8fca063e98c2c2981dbe99942e815da13078427c
kinstugi <kinstugi@webdev@gmail.com>
2022-07-22 11:05:33 +0300

    added the ability to move chess pieces

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,3 +1,24 @@
+class Move:
+
+    ranksToRow = {"1":7, "2":6, "3":5, "4": 4, "5":3, "6": 2, "7": 1, "8": 0}
+    rowsToRank = {v:k for k,v in ranksToRow.items()}
+    filesToCols = {"a":0, "b":1, "c":2, "d":3, "e": 4, "f":5, "g":6, "h": 7}
+    colsToFiles = {v:k for k,v in filesToCols.items()}
+
+    def __init__(self, start_square, end_square, board):
+        self.board = board
+        self.start_row, self.start_col = start_square
+        self.end_row, self.end_col = end_square
+        self.piece_moved = board[self.start_row][self.start_col]
+        self.piece_captured = board[self.end_row][self.end_col]
+    
+    def get_chess_notation(self):
+        return self.get_rank_file(self.start_row, self.start_col) + self.get_rank_file(self.end_row, self.end_col)
+
+    def get_rank_file(self, r,c):
+        return self.colsToFiles[c] + self.rowsToRank[r]
+
+
 class GameState:
     def __init__(self):
         self.board = [
@@ -13,3 +34,11 @@ class GameState:
 
         self.whiteToMove = True
         self.moveLog = []
+
+    def make_move(self, move: Move):
+        self.board[move.start_row][move.start_col] = '--'
+        self.board[move.end_row][move.end_col] = move.piece_moved
+        self.moveLog.append(move)
+        self.whiteToMove = not self.whiteToMove
+
+
--- ChessMain.py
+++ ChessMain.py
@@ -1,13 +1,11 @@
 import pygame as p
-from ChessEngine import GameState
+from ChessEngine import GameState, Move
 
 HEIGHT = WIDTH = 512
 DIMENSION = 8
 SQ_SIZE = HEIGHT//DIMENSION
 MAX_FPS = 15
 IMAGES = {}
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
 
 
 def load_images():
@@ -41,13 +39,33 @@ def main():
     gs = GameState()
     load_images()
     running = True
-    
+    selected_square = ()
+    player_clicks = []
+
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = p.mouse.get_pos()
+                c,r = mouse_x // SQ_SIZE, mouse_y // SQ_SIZE
+                if selected_square == (r,c):
+                    selected_square = ()
+                    player_clicks = []
+                else:
+                    selected_square = (r,c)
+                    player_clicks.append(selected_square)
+                
+                if len(player_clicks) == 2:
+                    chess_move = Move(player_clicks[0], player_clicks[1], gs.board)
+                    print(chess_move.get_chess_notation())
+                    gs.make_move(chess_move)
+                    selected_square = ()
+                    player_clicks = []
+
         draw_game_state(gs, screen)
         clock.tick(MAX_FPS)
         p.display.flip()
+
 if __name__ == "__main__":
     main()
